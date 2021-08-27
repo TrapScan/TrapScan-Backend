8ae09613738b8948655d346cb1d66cc60c0e9254
Dylan Hobbs <dylan@dylanhobbs.ie>
2021-08-27 10:43:22 +0300

    Fixing divide by zero

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -13,11 +13,12 @@ use Illuminate\Http\Request;
 class StatsController extends Controller
 {
     public function kpis() {
-        $total_users = User::count();
+        $total_users = User::count() ?? 1;
         $total_catches = Inspection::whereNotNull('species_caught')->count();
         $total_inspections = Inspection::count();
         $total_traps = Trap::count();
-        $total_projects = Project::count();
+        $total_projects = Project::count() ?? 1;
+
         $data = [
             'total_inspections' => $total_inspections,
             'total_catches' => $total_catches,
