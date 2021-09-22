423815f3f17ca0a961d704032573eaee0993378b
Dylan Hobbs <dhobbs@tcd.ie>
2021-09-22 13:50:20 +0300

    More mailgun config

--- config/mail.php
+++ config/mail.php
@@ -52,7 +52,8 @@ return [
         'mailgun' => [
             'transport' => 'mailgun',
             'domain' => env('MAILGUN_DOMAIN'),
-            'secret' => env('MAILGUN_SECRET')
+            'secret' => env('MAILGUN_SECRET'),
+            'endpoint' => env('MAILGUN_ENDPOINT')
         ],
 
         'postmark' => [
