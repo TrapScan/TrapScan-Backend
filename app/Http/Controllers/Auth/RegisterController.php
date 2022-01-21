2e3300ca8fb589e8f6de7027b2b02d61c8ef07a7
Dylan Hobbs <dhobbs@tcd.ie>
2022-01-21 09:03:42 +0200

    Switch to light mode as default for new user. Fixes #99

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,7 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $default_settings = ['theme' => 'default', 'dark_mode' => true];
+        $default_settings = ['theme' => 'default', 'dark_mode' => false];
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
